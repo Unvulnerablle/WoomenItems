fa4d3a5cfd3b9d04cd0d3713cbdd072e498f522d
Дмитрий Васильевич <zihc23@gmail.com>
2017-04-15 05:26:19 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
 
+
+//Calling functions navLink.
+$navlink = NEW navLink();
+$navlink ->_linkActive();
+
+//Connecting external files
+define('ROOT', dirname(__FILE__));
+require_once (ROOT. '/components/linkNavigation.php');
+
+//
